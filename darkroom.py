41dd4c8879433ead0fc964065fbb0269ee086301
Tom B <53711814+tombalcombe@users.noreply.github.com>
2019-10-27 22:20:03 +0000

    Update darkroom.py

--- darkroom.py
+++ darkroom.py
@@ -11,6 +11,7 @@ doublecheck = 0
 
 class Map:
     global nest
+    global level
     def __init__(self):
         self.area = []
         self.nest = []
@@ -31,7 +32,7 @@ class Map:
             print(i)
     def GeneratePlayer(self):
         print("================")
-        print("===DARK__ROOM===")
+        print("===DARK ROOM===")
         print("================")
         global hero
         self.area[-1][0] = 9
@@ -41,44 +42,50 @@ class Map:
         hero.health = random.randint(50,100)
         hero.attack = 1
         hero.equip = "bare fists"
-        print(f"Ok, your character name is {hero.name}")
+        print(f"Good luck, {hero.name}.")
         print(f"Your currently can do {hero.attack} damage")
         print(f"Your HP is {hero.health}")
     def GenerateMap(self, x):
         checker = -1 
         while checker <= x:
-            checker = checker+1
-            for yindex, i in enumerate(self.area):
-                xc = random.randint(0,4)
-                yc = random.randint(0,4)
-                if yindex == yc:
-                    for xindex, i in enumerate(self.area):
-                        if xindex == xc:
-                            self.area[yindex][xindex] = 1
+            try:
+                checker = checker+1
+                yc = random.randint(0,len(self.area))
+                xc = random.randint(0,len(self.area[0]))
+                print(xc,yc)
+                self.area[yc][xc] = 1
+            except:
+                self.area[-1][0] = 9
+                print(yc, xc, len(self.area),len(self.area[0]))
+        print(xc, yc, len(self.area),len(self.area[0]))
         self.area[-1][0] = 9
     def GenerateItems(self, x):
         checker = -1 
         while checker <= x:
-            checker = checker+1
-            for yindex, i in enumerate(self.area):
-                xc = random.randint(0,4)
-                yc = random.randint(0,4)
-                if yindex == yc:
-                    for xindex, i in enumerate(self.area):
-                        if xindex == xc:
-                            self.area[yindex][xindex] = random.randint(2,5) 
+            try:
+                checker = checker+1
+                yc = random.randint(0,len(self.area))
+                xc = random.randint(0,len(self.area[0]))
+                print(xc,yc)
+                self.area[yc][xc] = random.randint(2,8) 
+            except:
+                self.area[-1][0] = 9
+                print(yc, xc, len(self.area),len(self.area[0]))
+        print(xc, yc, len(self.area),len(self.area[0]))
         self.area[-1][0] = 9
     def GenerateEntity(self, x):
         checker = -1 
         while checker <= x:
-            checker = checker+1
-            for yindex, i in enumerate(self.area):
-                xc = random.randint(0,4)
-                yc = random.randint(0,4)
-                if yindex == yc:
-                    for xindex, i in enumerate(self.area):
-                        if xindex == xc:
-                            self.area[yindex][xindex] = random.randint(5,6) 
+            try:
+                checker = checker+1
+                yc = random.randint(0,len(self.area))
+                xc = random.randint(0,len(self.area[0]))
+                print(xc,yc)
+                self.area[yc][xc] = random.randint(10,12) 
+            except:
+                self.area[-1][0] = 9
+                print(yc, xc, len(self.area),len(self.area[0]))
+        print(xc, yc, len(self.area),len(self.area[0]))
         self.area[-1][0] = 9
     def MoveUp(self):
         print("You press on forwards into the unknown.")
@@ -148,10 +155,18 @@ class Map:
             for xindex, a in enumerate(i):
                 if self.area[yindex][xindex] != 9 and self.area[yindex][xindex] != 0:
                     return()
-        print("You find a door that leads to the outside. You have successfully escaped!")
-        print("You win.")
+        print("You find a door that leads to another area...")
+        newmap.Print()
+        print("You successfully completed this floor.")
+        print("You gained 2 points")
         time.sleep(2)
-        start()
+        newmap.Clear()
+        newmap.Build(random.randint(2,5),random.randint(2,5))  
+        newmap.GenerateMap(len(newmap.area)/4)
+        newmap.GenerateItems(len(newmap.area)/3)
+        newmap.GenerateEntity(len(newmap.area)/2)
+        level = level+2
+        game()
   
 def checkNumber(x):
     global hero
@@ -187,50 +202,61 @@ def checkNumber(x):
         print("The map suddenly sets itself alight and burns up!")
         return()
     if x == 2:
-        print("You have found a sword.")
-        hero.equip = "sword"
-        hero.attack = 5
-        print(f"You now have {hero.attack} attack")
+        PickItem("sword")
         return()
     if x == 3:
         print("A shadow shoots at you, knocking you off your feet.")
         print("You drop your weapon.")
-        hero.equip = "bare fists"
         hero.attack = 1
         print(f"You now have {hero.attack} attack")
         return()
     if x == 4:
-        print("You have found a broken lid of an ancient beer cask.")
-        print("It looks like it could be a makeshift shield")
-        hero.defense = 2
-        print(f"You now have {hero.defense} defense")
-        return()
-    if x == 4:
-        print("A beam of radiant light suddenly hits you and elates your spirit")
-        hero.health = hero.health + 50
-        hero.defense = hero.defense + 2
-        print(f"You now have {hero.defense} defense")
+        PickItem("shield")
         return()
     if x == 5:
+        PickItem("templar mace")
+        return()
+    if x == 6:
+        PickItem("dagger")
+        return()
+    if x == 7:
+        PickItem("excalibur")
+        return()
+    if x == 7:
+        PickItem("giant frying pan")
+        return()
+    if x == 10:
         print("You've found yourself a shadow beast")
         Fight("shadow beast")
         return()
-    if x == 6:
+    if x == 11:
         print("You've found yourself a small toad")
         Fight("toad")
         return()
-    if x == 7:
+    if x == 12:
         print("A goblin is alerted by your presence.")
         Fight("goblin")
         return()
+        
+def PickItem(x):
+    global hero
+    item = Item1.name.index(x)
+    hero.attack = hero.attack + Item1.attack[item]
+    hero.defense = hero.defense + Item1.defense[item]
+    print(f"You picked up a {Item1.name[item]}")
+    print(f"This gives you an extra {Item1.attack[item]} attack")
+    print(f"This also gives you an extra {Item1.defense[item]} defense")
+    return()
 
 def Fight(x):
+    global level
     global creature
     global doublecheck
     creature = Monster.name.index(x)
+    print(f"There's a {Monster.name[creature]} ahead!")
     print(f"The {Monster.name[creature]} lunges at you...")
-    creaturehealth = 30
-    creatureattack = 5
+    creaturehealth = Monster.health[creature]
+    creatureattack = Monster.attack[creature]
     while hero.health >= 0:
         hero.health = hero.health + (hero.defense-creatureattack)
         print(f"You take {hero.defense-Monster.attack[creature]} damage.")
@@ -239,16 +265,19 @@ def Fight(x):
         healthcheck()
         if creaturehealth <= 0:
             doublecheck = 0
-            print("You successfully killed the {Monster.name[creature]}.")
+            level = level + 1
+            print(f"You successfully killed the {Monster.name[creature]}.")
             print(f"You have {hero.health} hp remaining")
             return()
     print("You have died.")
+    print(f"Your final level was {level}")
     doublecheck = 0
     time.sleep(2)
+    start()
 
 def healthcheck():
     global doublecheck
-    if hero.health == 30 and doublecheck == 0:
+    if hero.health < 30 and doublecheck == 0:
         kk = input("You have dropped below 30 health, do you wish to continue fighting? [y] / [n]")
         if kk == 'y':
             doublecheck = 1
@@ -256,7 +285,17 @@ def healthcheck():
         if kk == 'n':
             print(f"You manage to run away from the {Monster.name[creature]}")
             doublecheck = 1
-            return()
+            game()
+            
+class Item:
+  def __init__(self):
+    self.name = []
+    self.defense = []
+    self.attack = []
+  def BuildItem(self,a,b,c):
+      self.name.append(a)
+      self.defense.append(b)
+      self.attack.append(c)
     
 class Monsta:
   def __init__(self):
@@ -264,11 +303,13 @@ class Monsta:
     self.health = []
     self.defense = []
     self.attack = []
-  def BuildMonster(self,a,b,c,d):
+    self.score = []
+  def BuildMonster(self,a,b,c,d,e):
       self.name.append(a)
       self.health.append(b)
       self.defense.append(c)
       self.attack.append(d)
+      self.score = e
 
 class Character:
   def __init__(self):
@@ -281,26 +322,40 @@ class Character:
 def start():
     global newmap
     global Monster
+    global Item1
+    global level
     Monster = Monsta()
-    Monster.BuildMonster("goblin",15,0,3)
-    Monster.BuildMonster("toad",10,0,1)
-    Monster.BuildMonster("shadow beast",20,0,5)
+    Item1 = Item()
+    Monster.BuildMonster("goblin",15,0,3,2)
+    Monster.BuildMonster("toad",10,0,1,1)
+    Monster.BuildMonster("shadow beast",20,0,5,3)
+    Item1.BuildItem("sword",0,5)
+    Item1.BuildItem("dagger",0,2)
+    Item1.BuildItem("shield",2,0)
+    Item1.BuildItem("templar mace",1,7)
+    Item1.BuildItem("excalibur",5,10)
+    Item1.BuildItem("giant frying pan",1,3)
+    Item1.BuildItem("excalibur",5,10)
     newmap = Map()
     newmap.Build(random.randint(2,5),random.randint(2,5))
+    print(len(newmap.area),len(newmap.area[0]))
     newmap.GeneratePlayer()    
-    newmap.GenerateMap(len(newmap.area)/8)
-    newmap.GenerateItems(len(newmap.area)/4)
-    newmap.GenerateEntity(len(newmap.area)/4)
-    newmap.Print()
-    print("You find yourself in a darkly lit corridor. You're not sure how you got here! But you know you must press on.")
+    newmap.GenerateMap(1)
+    newmap.GenerateItems(2)
+    newmap.GenerateEntity(3)
+    print("Your current score is 0. You gain points for killing monsters and completing levels.")
+    print("You find yourself in a darkly lit room...")
     print()
     print("You can move forwards, backwards, left or right.")
     print("Controls: [w] - forwards, [s] - backwards, [a] - left, [d] - right.")
     print("Press anything else to quit, i.e 'quit'.")
+    level = 0
     game()
                        
 def game():
+    global level
     global newmap
+    print(f"Your current score is: {level}")
     x = input("Choose your option:" )
     print("Controls: [w] - forwards, [s] - backwards, [a] - left, [d] - right.")
     print("Press anything else to quit, i.e 'quit'.")
@@ -318,6 +373,9 @@ def game():
         game()
     if x == "p":
         newmap.Print()
+        print(hero.health)
+        print(hero.attack)
+        print(hero.defense)
         game()
     else:
         quit()
